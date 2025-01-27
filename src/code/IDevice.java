cdbcdab07704802306e0adfd2183dc00909ce9b6
Kyle Cheon <kcheon@my.bcit.ca>
2025-01-27 12:43:26 -0800

    feat: create IDevice class

--- src/code/IDevice.java
+++ src/code/IDevice.java
@@ -1,2 +1,37 @@
-public class IDevice {
+public abstract class IDevice {
+
+    private final String purpose;
+
+    IDevice(final String purpose) {
+        validatePurpose(purpose);
+        this.purpose = purpose;
+    }
+
+    private final String getPurpose()
+    {
+        return this.purpose;
+    }
+
+    abstract void printDetails();
+
+    @Override
+    public abstract String toString();
+
+    @Override
+    public abstract boolean equals(Object obj);
+
+    @Override
+    public abstract int hashCode();
+
+    private static void validatePurpose(final String purpose) {
+
+        final boolean emptyString;
+
+        emptyString = purpose == null || purpose.isBlank();
+
+        if (emptyString)
+        {
+            throw new IllegalArgumentException("purpose cannot be null or empty");
+        }
+    }
 }
